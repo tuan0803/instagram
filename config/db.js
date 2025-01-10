3378ae079975ad763354bb52a889552afad1d59a
tuan0803 <nvt08032001@gmail.com>
2025-01-10 15:37:06 +0700

    update db config

--- config/db.js
+++ config/db.js
@@ -1,29 +1,24 @@
+const Sequelize = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
-const { Sequelize } = require('sequelize');
-
-
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,      
-    process.env.DB_USER,       
-    process.env.DB_PASSWORD,  
+    process.env.DB_DATABASE,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST,   
-        dialect: 'mysql',            
-        logging: false               
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: false,
     }
 );
 
-async function testConnection() {
-    try {
-        await sequelize.authenticate();
-        console.log('Database connected successfully!');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-        process.exit(1); 
-    }
-}
-
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connected Successfully');
+    })
+    .catch(e => {
+        console.log('Connection Error: ', e);
+    });
 
 module.exports = sequelize;
