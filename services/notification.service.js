e90f8ea70a2cbacbaf81f8c00507de603b1c8318
tuan0803 <74147311+tuan0803@users.noreply.github.com>
2025-01-13 09:18:56 +0700

    Update notification.service.js

--- services/notification.service.js
+++ services/notification.service.js
@@ -1,24 +1,44 @@
 
 const db = require('../config/db'); 
-
+const Notification = require('../models/Notification')
 class NotificationService {
   
-  async createNotification(userId, content) {
-    const query = `INSERT INTO notification (user_id, content) VALUES (?, ?)`;
-    await db.execute(query, [userId, content]);
+  async createNotification(user_id, content) {
+        try {
+            const notification = await Notification.create({
+                user_id : user_id,
+                content : content
+            });
+            return notification;
+        } catch (error) {
+            throw new Error('Error create Notification'+ error.message);
+        }
   }
 
   
-  async getNotifications(userId) {
-    const query = `SELECT * FROM notification WHERE user_id = ? ORDER BY created_at DESC`;
-    const [notifications] = await db.execute(query, [userId]);
-    return notifications;
+  async getNotifications(user_id) {
+    try {
+        const notification = await Notification.findAll({
+            where: { user_id: user_id },
+            order: { created_at: 'DESC' }
+        });
+        return notification;
+    } catch (error) {
+            throw new Error('Cant get Notification'+ error.message);
+    }
   }
 
   
-  async markAsRead(notificationId) {
-    const query = `UPDATE notification SET status = 'read' WHERE notification_id = ?`;
-    await db.execute(query, [notificationId]);
+  async markAsRead(notification_id) {
+    try {
+        const notification = await Notification.update(
+            { status : read},
+            { where: { notification_id: notification_id } }
+        );
+        return notification;
+    } catch (error) {
+            throw new Error('Error mark'+ error.message);
+    }
   }
 }
 
